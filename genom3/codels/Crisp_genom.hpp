87fde46f6a373a48fb9002a83361694e7544c220
Pierre Narvor <pnarvor@laas.fr>
2018-04-17 16:21:16 +0200

    (Splitted from CDFF_at_LAAS)[Crisp-rebase] Compiled crisp-genom

--- genom3/codels/Crisp_genom.hpp
+++ genom3/codels/Crisp_genom.hpp
@@ -1,7 +1,7 @@
 #ifndef DEF_CRISP_GENOM_H
 #define DEF_CRISP_GENOM_H
 
-#include "../../../src/CrispASN1.hpp"
+#include <CrispASN1.hpp>
 
 struct Crisp_genom : public PositionManager::CrispASN1
 {};
